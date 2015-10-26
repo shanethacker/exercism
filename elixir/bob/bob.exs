466b3db31ad7d1b852becec1d59b494a5450bf10
Shane Thacker <sst@clockworkmind.com>
2015-10-26 12:47:03 -0400

    Passing tests
    Originally had it passing just using a conditional, but I decided to
    work with pattern matching as well.

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,8 +1,33 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
+    	String.strip(input) == ""
+    		-> reply(:silence)
 
+    	String.ends_with?(input, "?")
+        	-> reply(:question)
+
+        String.upcase(input) == input && String.downcase(input) != input
+        	-> reply(:yell)
+
+        true 
+        	-> reply(:else)
     end
+  end
+
+  defp reply(:silence) do
+  	"Fine. Be that way!"
+  end
+
+  defp reply(:question) do
+  	"Sure."
+  end
+
+  defp reply(:yell) do
+  	"Whoa, chill out!"
+  end
+
+  defp reply(_) do
+  	"Whatever."
   end
 end
