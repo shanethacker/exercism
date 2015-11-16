e61e8d534191ed6867e7d57377765d01ca76ec5b
Shane Thacker <sst@clockworkmind.com>
2015-11-16 12:58:53 -0500

    Fixed spaces.

--- elixir/gigasecond/gigasecond.exs
+++ elixir/gigasecond/gigasecond.exs
@@ -1,13 +1,13 @@
 defmodule Gigasecond do
-	@doc """
-	Calculate a date one billion seconds after an input date.
-	"""
-	@spec from({pos_integer, pos_integer, pos_integer}) :: :calendar.date
+  @doc """
+  Calculate a date one billion seconds after an input date.
+  """
+  @spec from({pos_integer, pos_integer, pos_integer}) :: :calendar.date
 
-	def from({year, month, day}) do
+  def from({year, month, day}) do
     :calendar.datetime_to_gregorian_seconds({{year, month, day}, {0, 0, 0}})
     |> + 1000000000
     |> :calendar.gregorian_seconds_to_datetime
     |> elem(0)
-	end
+  end
 end
